002ed6521c9374e9f9fe4cfdd2e51180e6b4537b
Ali.Agbaria <Ali.Agbaria@drklein.de>
2018-11-29 16:34:21 +0100

    SSH-Programm eingebettet

--- ServiceMgmtPlugin/de/vassiliougioles/ttcn/ttwb/functions/ServiceMgmtPlugin.java
+++ ServiceMgmtPlugin/de/vassiliougioles/ttcn/ttwb/functions/ServiceMgmtPlugin.java
@@ -5,7 +5,13 @@ import org.etsi.ttcn.tci.RecordValue;
 
 import com.testingtech.ttcn.annotation.ExternalFunction;
 import com.testingtech.ttcn.tri.AnnotationsExternalFunctionPlugin;
-
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import java.util.Properties;
+import java.io.InputStream;
 
 @ExternalFunction.Definitions(ServiceMgmtPlugin.class)
 public class ServiceMgmtPlugin extends AnnotationsExternalFunctionPlugin {
@@ -27,8 +33,12 @@ public class ServiceMgmtPlugin extends AnnotationsExternalFunctionPlugin {
 	 @ExternalFunction(name = "stopService", module = "Lib_ServiceManagement")
 	  public CharstringValue stopService(RecordValue serviceSpec) {
 		 
-		 logInfo("WE ARE IN EXTERNAL FUNCTIONS STOP");
-		 return null; 
+		 System.out.println("WE ARE IN EXTERNAL FUNCTIONS STOP");
+		 //logInfo("WE ARE IN EXTERNAL FUNCTIONS STOP");
+		 
+
+
+		 return newCharstringValue("This is a RESULT");  
 	 }
 	 
 	 
@@ -36,6 +46,61 @@ public class ServiceMgmtPlugin extends AnnotationsExternalFunctionPlugin {
 	  public CharstringValue getServiceStatus(RecordValue serviceSpec) {
 		 
 		 logInfo("WE ARE IN EXTERNAL FUNCTIONS GETSERVICESTATUS");
-		 return newCharstringValue("running"); 
+		 System.out.println("WE ARE IN EXTERNAL FUNCTIONS GETSERVICESTATUS");
+		 try {
+	            JSch jsch = new JSch();
+
+	            String user = "wowi";
+	            String host = "solv116";
+	            int port = 22;
+	            String privateKey = "H:\\ssh/id_rsa";
+	            String command = " sudo  /etc/init.d/mywowi-login-gateway status";
+
+
+
+	            jsch.addIdentity(privateKey);
+	            System.out.println("identity added ");
+
+	            Session session = jsch.getSession(user, host, port);
+	            System.out.println("session created.");
+
+	            java.util.Properties config = new java.util.Properties();
+
+	            config.put("StrictHostKeyChecking", "no");
+	            session.setConfig(config);
+	            session.connect();
+	            System.out.println("session connected.....");
+	            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+	            channel.setCommand(command);
+	            channel.setPty(true);
+	            channel.setErrStream(System.err);
+	            InputStream in = channel.getInputStream();
+	            channel.connect();
+	            System.out.println("Connected...");
+	            byte[] tmp = new byte[1024];
+	            while (true) {
+	                while (in.available() > 0) {
+	                    int i = in.read(tmp, 0, 1024);
+	                    if (i < 0) {
+	                        break;
+	                    }
+	                    System.out.print(new String(tmp, 0, i));
+	                }
+	                if (channel.isClosed()) {
+	                    System.out.println("Exit Status: "
+	                            + channel.getExitStatus());
+	                    break;
+	                }
+	                Thread.sleep(1000);
+	            }
+	            channel.disconnect();
+	            session.disconnect();
+	            System.out.println("DONE!!!");
+
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		 return newCharstringValue("This is a RESULT"); 
 	 }
 }
