936062ea0fbc11ecbb2581dcb2a4e786455e1777
Ali.Agbaria <ali.agbaria@mywowi.de>
2018-11-30 18:58:33 +0100

    Die Methoden start und stop vorbereitet.

--- ServiceMgmtPlugin/de/vassiliougioles/ttcn/ttwb/functions/ServiceMgmtPlugin.java
+++ ServiceMgmtPlugin/de/vassiliougioles/ttcn/ttwb/functions/ServiceMgmtPlugin.java
@@ -2,6 +2,7 @@ package de.vassiliougioles.ttcn.ttwb.functions;
 
 import org.etsi.ttcn.tci.CharstringValue;
 import org.etsi.ttcn.tci.RecordValue;
+import org.etsi.ttcn.tci.Value;
 
 import com.testingtech.ttcn.annotation.ExternalFunction;
 import com.testingtech.ttcn.tri.AnnotationsExternalFunctionPlugin;
@@ -21,40 +22,235 @@ public class ServiceMgmtPlugin extends AnnotationsExternalFunctionPlugin {
 
 		 
 		 String[] fieldNames = serviceSpec.getFieldNames();
-		 
+		/* 
 		 for (String string : fieldNames) {
 			 System.out.println("The field name is " + string + " and the values are " + serviceSpec.getField(string));
 		}
+		
+		 System.out.println(serviceSpec.getField(fieldNames[1]));
+		 System.out.println(serviceSpec.getField(fieldNames[3]));
+		 System.out.println(serviceSpec.getField(fieldNames[3]));
+		 */
+		 String host= ""+ serviceSpec.getField(fieldNames[1]);
+		 String user= ""+ serviceSpec.getField(fieldNames[2]);
+		 
+		 RecordValue authlist = (RecordValue) serviceSpec.getField(fieldNames[3]);
+		 String[] auths= authlist.getFieldNames();
+		 
+		 String auth= ""+ authlist.getField(auths[1]);
+		 
+		 RecordValue commandlist = (RecordValue) serviceSpec.getField(fieldNames[4]);
+		 String[] commands= commandlist.getFieldNames();
+		 
+		 String command= ""+ commandlist.getField(commands[0]);
+		 
+		 //String command =""+  serviceSpec.getField(fieldNames[4]);
+		 
+		 System.out.println("Host ist "+host);
+		 System.out.println("user ist "+user);
+		 System.out.println("auth  "+auth);
+		 System.out.println("command  "+command);
+		 
+		 
+		 /*
+			try {
+		            JSch jsch = new JSch();
 
+		            //String user = "wowi";
+		            //String host = "solv116";
+		            int port = 22;
+		            String privateKey = auth;
+		            //String command = " sudo  /etc/init.d/mywowi-login-gateway status";
+
+
+	
+		            jsch.addIdentity(privateKey);
+		            System.out.println("identity added ");
+	
+		            Session session = jsch.getSession(user, host, port);
+		            System.out.println("session created.");
+	
+		            java.util.Properties config = new java.util.Properties();
+	
+		            config.put("StrictHostKeyChecking", "no");
+		            session.setConfig(config);
+		            session.connect();
+		            System.out.println("session connected.....");
+		            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+		            channel.setCommand(command);
+		            channel.setPty(true);
+		            channel.setErrStream(System.err);
+		            InputStream in = channel.getInputStream();
+		            channel.connect();
+		            System.out.println("Connected...");
+		            byte[] tmp = new byte[1024];
+		            while (true) {
+		                while (in.available() > 0) {
+		                    int i = in.read(tmp, 0, 1024);
+		                    if (i < 0) {
+		                        break;
+		                    }
+		                    System.out.print(new String(tmp, 0, i));
+		                }
+		                if (channel.isClosed()) {
+		                    System.out.println("Exit Status: "
+		                            + channel.getExitStatus());
+		                    break;
+		                }
+		                Thread.sleep(1000);
+		            }
+		            channel.disconnect();
+		            session.disconnect();
+		            System.out.println("DONE!!!");
+	
+	
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }*/
+			
 		 return newCharstringValue("This is a RESULT"); 
 	 }
 	 
 	 
 	 @ExternalFunction(name = "stopService", module = "Lib_ServiceManagement")
 	  public CharstringValue stopService(RecordValue serviceSpec) {
-		 
-		 System.out.println("WE ARE IN EXTERNAL FUNCTIONS STOP");
-		 //logInfo("WE ARE IN EXTERNAL FUNCTIONS STOP");
-		 
+		 String[] fieldNames = serviceSpec.getFieldNames();
+			/* 
+			 for (String string : fieldNames) {
+				 System.out.println("The field name is " + string + " and the values are " + serviceSpec.getField(string));
+			}
+			
+			 System.out.println(serviceSpec.getField(fieldNames[1]));
+			 System.out.println(serviceSpec.getField(fieldNames[3]));
+			 System.out.println(serviceSpec.getField(fieldNames[3]));
+			 */
+			 String host= ""+ serviceSpec.getField(fieldNames[1]);
+			 String user= ""+ serviceSpec.getField(fieldNames[2]);
+			 
+			 RecordValue authlist = (RecordValue) serviceSpec.getField(fieldNames[3]);
+			 String[] auths= authlist.getFieldNames();
+			 
+			 String auth= ""+ authlist.getField(auths[1]);
+			 
+			 RecordValue commandlist = (RecordValue) serviceSpec.getField(fieldNames[4]);
+			 String[] commands= commandlist.getFieldNames();
+			 
+			 String command= ""+ commandlist.getField(commands[1]);
+			 
+			 //String command =""+  serviceSpec.getField(fieldNames[4]);
+			 
+			 System.out.println("Host ist "+host);
+			 System.out.println("user ist "+user);
+			 System.out.println("auth  "+auth);
+			 System.out.println("command  "+command);
+			 
+			 
+			 /*
+			 try {
+		            JSch jsch = new JSch();
+
+		            //String user = "wowi";
+		            //String host = "solv116";
+		            int port = 22;
+		            String privateKey = auth;
+		            //String command = " sudo  /etc/init.d/mywowi-login-gateway status";
 
 
-		 return newCharstringValue("This is a RESULT");  
+
+		            jsch.addIdentity(privateKey);
+		            System.out.println("identity added ");
+
+		            Session session = jsch.getSession(user, host, port);
+		            System.out.println("session created.");
+
+		            java.util.Properties config = new java.util.Properties();
+
+		            config.put("StrictHostKeyChecking", "no");
+		            session.setConfig(config);
+		            session.connect();
+		            System.out.println("session connected.....");
+		            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+		            channel.setCommand(command);
+		            channel.setPty(true);
+		            channel.setErrStream(System.err);
+		            InputStream in = channel.getInputStream();
+		            channel.connect();
+		            System.out.println("Connected...");
+		            byte[] tmp = new byte[1024];
+		            while (true) {
+		                while (in.available() > 0) {
+		                    int i = in.read(tmp, 0, 1024);
+		                    if (i < 0) {
+		                        break;
+		                    }
+		                    System.out.print(new String(tmp, 0, i));
+		                }
+		                if (channel.isClosed()) {
+		                    System.out.println("Exit Status: "
+		                            + channel.getExitStatus());
+		                    break;
+		                }
+		                Thread.sleep(1000);
+		            }
+		            channel.disconnect();
+		            session.disconnect();
+		            System.out.println("DONE!!!");
+
+
+		        } catch (Exception e) {
+		            e.printStackTrace();
+		        }*/
+			
+			 return newCharstringValue("This is a RESULT"); 
+		 
+		 
 	 }
 	 
 	 
 	 @ExternalFunction(name = "getServiceStatus", module = "Lib_ServiceManagement")
 	  public CharstringValue getServiceStatus(RecordValue serviceSpec) {
 		 
-		 logInfo("WE ARE IN EXTERNAL FUNCTIONS GETSERVICESTATUS");
-		 System.out.println("WE ARE IN EXTERNAL FUNCTIONS GETSERVICESTATUS");
+		 String[] fieldNames = serviceSpec.getFieldNames();
+			/* 
+			 for (String string : fieldNames) {
+				 System.out.println("The field name is " + string + " and the values are " + serviceSpec.getField(string));
+			}
+			
+			 System.out.println(serviceSpec.getField(fieldNames[1]));
+			 System.out.println(serviceSpec.getField(fieldNames[3]));
+			 System.out.println(serviceSpec.getField(fieldNames[3]));
+			 */
+			 String host= ""+ serviceSpec.getField(fieldNames[1]);
+			 String user= ""+ serviceSpec.getField(fieldNames[2]);
+			 
+			 RecordValue authlist = (RecordValue) serviceSpec.getField(fieldNames[3]);
+			 String[] auths= authlist.getFieldNames();
+			 
+			 String auth= ""+ authlist.getField(auths[1]);
+			 
+			 RecordValue commandlist = (RecordValue) serviceSpec.getField(fieldNames[4]);
+			 String[] commands= commandlist.getFieldNames();
+			 
+			 String command= ""+ commandlist.getField(commands[2]);
+			 
+			 //String command =""+  serviceSpec.getField(fieldNames[4]);
+			 
+			 System.out.println("Host ist "+host);
+			 System.out.println("user ist "+user);
+			 System.out.println("auth  "+auth);
+			 System.out.println("command  "+command);
+			 
+			 
+		 
+		 
 		 try {
 	            JSch jsch = new JSch();
 
-	            String user = "wowi";
-	            String host = "solv116";
+	          //String user = "wowi";
+	            //String host = "solv116";
 	            int port = 22;
-	            String privateKey = "H:\\ssh/id_rsa";
-	            String command = " sudo  /etc/init.d/mywowi-login-gateway status";
+	            String privateKey = auth;
+	            //String command = " sudo  /etc/init.d/mywowi-login-gateway status";
 
 
 
